e530749d58217f221a4acb893f5b517431e102dc
BenTScott <bentysonscott@hotmail.co.uk>
2021-08-19 18:11:03 +0100

    Github action test 2

--- infrastructure/lib/hello-cdk-stack.ts
+++ infrastructure/lib/hello-cdk-stack.ts
@@ -6,7 +6,7 @@ export class HelloCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     new s3.Bucket(this, 'TestBucket', {
-      versioned: true
+      versioned: false
     });
   }
 }

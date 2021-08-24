271e622c104f8f4de09f0c95a8a1ccb7334b947d
BenTScott <bentysonscott@hotmail.co.uk>
2021-08-24 09:19:19 +0100

    Add second service

--- infrastructure/lib/api-chart.ts
+++ infrastructure/lib/api-chart.ts
@@ -25,11 +25,29 @@ export class ApiChart extends Chart {
       })
     );
 
-    var service = deployment.expose(8080, {
+    var service = deployment.expose(80, {
       serviceType: ServiceType.CLUSTER_IP,
     });
 
     const ingress = new Ingress(this, "ApiIngress");
-    ingress.addRule("/", IngressBackend.fromService(service));
+    ingress.addRule("/api", IngressBackend.fromService(service));
+
+    var deployment2 = new Deployment(this, "ApiDeployment2", {
+      replicas: 1,
+    });
+
+    deployment2.addContainer(
+      new Container({
+        image: "stefanprodan/podinfo",
+        port: 9898,
+      })
+    );
+
+    var service2 = deployment.expose(8080, {
+      serviceType: ServiceType.CLUSTER_IP,
+    });
+
+    const ingress2 = new Ingress(this, "ApiIngress2");
+    ingress2.addRule("/", IngressBackend.fromService(service2));
   }
 }

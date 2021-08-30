fc4373a113962dbe1a5cccf52f24dfbfc1affe8b
BenTScott <bentysonscott@hotmail.co.uk>
2021-08-30 14:18:32 +0100

    Add test lambda endpoint deploy

---
+++ api/cmd/users/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello World",
+	}, nil
+}
